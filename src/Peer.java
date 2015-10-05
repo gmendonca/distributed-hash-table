f189001d50e11aa04650d4a44ff36619c5995a09
Gustavo Mendonca <gustavogmend@gmail.com>
2015-10-05 16:20:56 -0500

    cleaning code

--- src/Peer.java
+++ src/Peer.java
@@ -1,19 +1,8 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import util.PeerQueue;
-import util.Util;
 
 
 public class Peer {
